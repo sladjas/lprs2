5b4aea6d9a8f00613791a6334acc3acff0105c29
sladjas <107408094+sladjas@users.noreply.github.com>
2022-06-15 17:29:31 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+#include<string>
+#include<stdlib.h>
+#include"SerialPort.h"
+
+char output[MAX_DATA_LENGTH];
+char incomingData[MAX_DATA_LENGTH];
+
+char *port = "\\\\.\\COM6";
+
+int main(){
+	SerialPort arduino(port);
+	if(arduino.isConnected()){
+		cout<<"Connection made"<<endl<<endl;
+	}
+	else{
+		cout<<"Error in port name"<<endl<<endl;
+	}
+	while(arduino.isConnected()){
+		cout<<"Enter your number: "<<endl;
+		string data;
+		cin>>data;
+
+		char *charArray = new char[data.size() + 1];
+		copy(data.begin(), data.end(), charArray);
+		charArray[data.size()] = '\n';
+
+		arduino.writeSerialPort(charArray, MAX_DATA_LENGTH);
+		arduino.readSerialPort(output, MAX_DATA_LENGTH);
+
+		cout<<">> "<<output<<endl;
+
+		delete [] charArray;
+	}
+	return 0;
+}

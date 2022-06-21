3b84f81067087232fdd63dfda494a9a8b46a40b9
sladjas <107408094+sladjas@users.noreply.github.com>
2022-06-21 14:22:29 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,36 +2,73 @@
 using namespace std;
 #include<string>
 #include<stdlib.h>
+#include <ctype.h>
+#include<windows.h>
+
+
 #include"SerialPort.h"
+#include <algorithm>
 
 char output[MAX_DATA_LENGTH];
 char incomingData[MAX_DATA_LENGTH];
 
-char *port = "\\\\.\\COM6";
+const char *port = "COM6";
 
-int main(){
+bool is_digits(const std::string& str)
+{
+	return std::all_of(str.begin(), str.end(), std::isdigit);
+}
+
+int main() {
 	SerialPort arduino(port);
-	if(arduino.isConnected()){
-		cout<<"Connection made"<<endl<<endl;
+	if (arduino.isConnected()) {
+		cout << "Arduino je povezan" << endl << endl;
 	}
-	else{
-		cout<<"Error in port name"<<endl<<endl;
+	else {
+		cout << "Pogresan PORT" << endl << endl;
 	}
-	while(arduino.isConnected()){
-		cout<<"Enter your number: "<<endl;
+	while (arduino.isConnected()) {
+		cout << "Unesite broj koji zelite da se ispise na displeju: " << endl;
 		string data;
-		cin>>data;
+		cin >> data;
+		fflush(stdout);
+		Sleep(1000);
 
-		char *charArray = new char[data.size() + 1];
-		copy(data.begin(), data.end(), charArray);
-		charArray[data.size()] = '\n';
+		if(data.size()>8){
+			cout << "Broj ne smije imati vise od 8 cifara!" << endl << endl;
+		}
+		else {
+			if (is_digits(data)) {
+				char* charArray = new char[data.size() + 1];
+				copy(data.begin(), data.end(), charArray);
+				charArray[data.size()] = '\n';
 
-		arduino.writeSerialPort(charArray, MAX_DATA_LENGTH);
-		arduino.readSerialPort(output, MAX_DATA_LENGTH);
+				arduino.writeSerialPort(charArray, MAX_DATA_LENGTH);
+				arduino.readSerialPort(output, MAX_DATA_LENGTH);
 
-		cout<<">> "<<output<<endl;
+				delete[] charArray;
+				data = "";
+				data.clear();
 
-		delete [] charArray;
+				cout << "Da li zelite jos brojeva?" << endl << endl;
+				cout << "Ako je vas odgovor DA unesite y." << endl;
+				cout << "Ako je vas odgovor NE unesite n." << endl;
+				string data1;
+				cin >> data1;
+				while (data1.compare("y") != 0 && data1.compare("n") != 0) {
+					cout << "Pogresan unos, unesite ponovo" << endl << endl;
+					cin >> data1;
+				}
+				if (data1.compare("n") == 0) {
+					cout << "Dovidjenja" << endl << endl;
+					break;
+				}
+			}
+			else {
+				cout << "Pogresan unos, unesite broj" << endl << endl;
+			}
+		}
+	
 	}
 	return 0;
 }
